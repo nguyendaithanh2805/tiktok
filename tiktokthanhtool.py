1a08067bed08980e8c7e22f57ef47bb0c7586c1d
nguyendaithanh2805 <93860196+nguyendaithanh2805@users.noreply.github.com>
2023-10-17 23:49:43 +0700

    Update tiktokthanhtool.py

--- tiktokthanhtool.py
+++ tiktokthanhtool.py
@@ -1,28 +1,155 @@
-import requests, html
-def get(cookie):
-    headers = {
-        'authority' : 'anotepad.com',
-        'method' : 'GET',
-        'path' :  '/notes/3f3rdkh5',
-        'scheme' : 'https',
-        'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Accept-Language' : 'vi,vi-VN;q=0.9,en;q=0.8',
-        'Cache-Control' : 'max-age=0',
-        'Cookie' : cookie,
-        'Referer' : 'https://anotepad.com/',
-        'Sec-Ch-Ua' :    '"Chromium";v="118", "Google Chrome";v="118", "Not=A?Brand";v="99"',
-        'Sec-Ch-Ua-Mobile' : '?0',
-        'Sec-Ch-Ua-Platform' : '"Windows"',
-        'Sec-Fetch-Dest' : 'document',
-        'Sec-Fetch-Mode' : 'navigate',
-        'Sec-Fetch-Site' : 'same-origin',
-        'Sec-Fetch-User' : '?1',
-        'Upgrade-Insecure-Requests' : '1',
-        'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
-    }
-    url = requests.get('https://anotepad.com/notes/3f3rdkh5', headers= headers)
-    text = url.text
-    split = text.split('<textarea name="notecontent" class="form-control textarea " id="edit_textarea" placeholder="Note Content" tabindex="2" rows="19">')[1].split('</textarea>')[0]
-    exec(html.unescape(split))
-cookie ='_ga=GA1.1.1560674324.1697271489; AnotepadId=CD98EA37B69AF0656EFFB8DC4A1B6C361093EF61D45785921782752710C564F5D293AA11C0CC34BBEBED7417EC517E61F9A8A8442C8EC04896F0FDB1491F44B206197B4136B17612D693EF4FAB2D8436CB7EC2F550E99FA8E475011804148960B0059E9AECB8F2550B659B2D3B955D1B; _ga_6PG3MM86KX=GS1.1.1697553822.5.1.1697553901.0.0.0'
-get(cookie)
+import requests, os
+import requests, json, time, datetime, re
+class OOP:
+    def __init__(self, TDS_token, idtiktok):
+        self.TDS_token = TDS_token
+        self.idtiktok = idtiktok
+        self.demNV = 0
+        self.xuHienTai = 0
+        self.STT = 0
+        self.s = requests.Session()
+    def layThongTinAcc(self):
+        url = 'https://traodoisub.com/api/?fields=profile&access_token={0}'.format(self.TDS_token)
+        response = self.s.get(url)
+        dataLTT =  json.loads(response.text)
+        if('error' in dataLTT):
+            print('Token tds die !!!')
+            exit()
+        else:
+            user = dataLTT['data']['user']
+            xu = dataLTT['data']['xu']
+            xudie = dataLTT['data']['xudie']
+            self.xuHienTai += int(xu)
+            print(f'User : {user} | Xu : {xu} | Xu die : {xudie}')
+    def datCauHinh(self):
+        url = 'https://traodoisub.com/api/?fields=tiktok_run&id={0}&access_token={1}'.format(self.idtiktok, self.TDS_token)
+        response = self.s.get(url)
+        dataDCH = json.loads(response.text)
+        if('error' in dataDCH):
+            print('Kiểm tra lại xem cấu hình tiktok chưa')
+            exit()
+        else:
+            id = dataDCH['data']['id']
+            user = dataDCH['data']['uniqueID']
+            msg = dataDCH['data']['msg']
+            print(f'{id} | {user} | {msg}')
+    def layNhiemVu(self):
+        while(True):
+            url = 'https://traodoisub.com/api/?fields=tiktok_follow&access_token={}'.format(self.TDS_token)
+            response = self.s.get(url)
+            try:
+                data = json.loads(response.text)
+                if 'countdown' in data:
+                        countdown_value = data['countdown']
+                        for i in range(int(countdown_value) + 5, 0, -1):
+                            print(f'Thao tác quá nhanh vui lòng chậm lại, đợi {i} giây', end='\r')
+                            time.sleep(1)
+                        print(" " * 50, end='\r')
+                elif 'time_reset' in data:
+                    print('Đổi tiktok mới, bị giới hạn nhiệm vụ rồi')
+                    exit()
+                else:
+                    for item in data['data']:
+                        link_value = item['link']
+                        id_value = item['id']
+                        self.guiNhiemVu(id_value)
+                        if (self.STT == answer):
+                            self.nghiChongBlock(chongBlock)
+                        else:
+                            self.delay(seconds)
+                        self.follow(link_value)
+                        self.demNV += 1
+                        if self.demNV == 9:
+                            self.nhanXu()
+                            self.demNV = 0
+                            continue
+                        # for i in range(len(arr_link_value)):
+                        #     link_value = arr_link_value[i]
+                        #     print(link_value)
+                            # id_value = arr_id_value[i]
+                            # self.guiNhiemVu(id_value)
+                            # time.sleep(5)
+                            # self.follow(link_value)
+
+            except json.JSONDecodeError:
+                print("Error decoding JSON response.")
+    def guiNhiemVu(self, id_value):
+        url = 'https://traodoisub.com/api/coin/?type=TIKTOK_FOLLOW_CACHE&id={0}&access_token={1}'.format(id_value, self.TDS_token)
+        response = self.s.get(url)
+        dataGNV = response.json()
+        
+        # for i in range(1, dataGNV['cache'] + 1):
+        #     self.demNV += 1
+        #     if self.demNV == 9:
+        #         self.nhanXu()
+        #         self.demNV = 0
+        #         continue
+    def follow(self, link_value):
+       os.system(f'termux-open-url {link_value}')
+    def nhanXu(self):
+     
+            now = datetime.datetime.now()
+            url = 'https://traodoisub.com/api/coin/?type=TIKTOK_FOLLOW&id=TIKTOK_FOLLOW_API&access_token={}'.format(self.TDS_token)
+            response = self.s.get(url)
+            dataNX = response.json()
+            if response.status_code == 200:
+                if 'data' in dataNX:
+                    xu = dataNX['data']['xu']
+                    job_success = dataNX['data']['job_success']
+                    # xuthem = dataNX['data']['xu_them']
+                    msg = dataNX['data']['msg']
+                    xuTong = int(re.search(r'\d+', xu).group())
+                    self.xuHienTai += xuTong
+                    self.STT+= 1
+                    print(f"[{self.STT}] | {now.strftime('%H:%M:%S')} |  Số job đã làm : {job_success} | THÀNH TOOL | {msg} | {xuTong} | Xu hiện tại : {self.xuHienTai}")
+                else:
+                    print("Nhan xu that bai")
+            else:
+                print(f"Yêu cầu không thành công. Mã trạng thái: {response.status_code}")
+        
+    def delay(self, seconds):
+        for i in range(seconds, 0, -1):
+            print(f'Vui lòng đợi sau -> {str(i)} giây', end='\r')
+            time.sleep(1)
+    def nghiChongBlock(self, chongBlock):
+        for i in range(chongBlock, 0, -1):
+            print(f'Đang nghỉ chống block, vui lòng đợi sau -> {str(i)} giây', end='\r')
+            time.sleep(1)
+def save_account_info(TDS_token, idtiktok):
+    with open("tds_token.txt", "w") as tds_file:
+        tds_file.write(TDS_token)
+    with open("idtiktok.txt", "w") as idtiktok_file:
+        idtiktok_file.write(idtiktok)
+
+def load_account_info():
+    try:
+        with open("tds_token.txt", "r") as tds_file:
+            TDS_token = tds_file.read()
+        with open("idtiktok.txt", "r") as idtiktok_file:
+            idtiktok = idtiktok_file.read()
+        return TDS_token, idtiktok
+    except FileNotFoundError:
+        return None, None
+TDS_token, idtiktok = load_account_info()
+if TDS_token is None or idtiktok is None:
+    TDS_token = input('Nhập token TDS : ')
+    idtiktok = input('Nhập id tiktok cần cấu hình : ')
+    save_account_info(TDS_token, idtiktok)
+else:
+    keep_old_token = input('Bạn có muốn giữ lại token TDS cũ không? (y/n): ')
+    if keep_old_token.lower() != 'y':
+        TDS_token = input('Nhập token TDS mới: ')
+
+    keep_old_idtiktok = input('Bạn có muốn giữ lại id tiktok cũ không? (y/n): ')
+    if keep_old_idtiktok.lower() != 'y':
+        idtiktok = input('Nhập id tiktok mới: ')
+    save_account_info(TDS_token, idtiktok)
+seconds = int(input('Nhập delay : '))
+answer = int(input('Sau bao nhiêu nhiệm vụ thì nghỉ chống block : '))
+chongBlock = int(input('Nghỉ chống block bao nhiêu giây : '))
+# os.system('termux-open-url https:\/\/tiktok.com\/@nguyenngocquang004')
+# TDS_token = 'TDSQfikjclZXZzJiOiIXZ2V2ciwiIxETMxgmbhhGdpFGZiojIyV2c1Jye'
+# idtiktok = '7170579645727867931'
+api = OOP(TDS_token, idtiktok)
+api.datCauHinh()
+api.layNhiemVu()
